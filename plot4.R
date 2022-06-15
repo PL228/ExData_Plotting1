45e46f92a44f9f17f00d4a222f77f83c4bc4218f
PL228 <peiklui@yahoo.com>
2022-06-15 21:38:19 +0800

    commit plot4

---
+++ plot4.R
@@ -0,0 +1,47 @@
+zipfile <- "exdata_data_household_power_consumption.zip"
+
+# Checking if archieve already exists.
+if (!file.exists(zipfile)){
+        fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(fileURL, zipfile, method="curl")
+}  
+
+datafile <- "household_power_consumption.txt"
+if (!file.exists(datafile)){
+        unzip(filename) 
+}  
+
+# Read the data into table
+exdata <- read.table(datafile,header=TRUE, sep=";",)
+
+# Convert "?" to NA
+exdata[exdata == "?"] <- NA
+
+# Selecting 2 days data 2007-02-01 and 2007-02-02
+exdata$Date <- as.Date(exdata$Date, format = "%d/%m/%Y")
+two_days_data <- exdata[exdata$Date >= as.Date("2007-02-01") & exdata$Date <= as.Date("2007-02-02"),]
+
+# Joining date and time to create a new posix date
+two_days_data$posix <- as.POSIXct(strptime(paste(two_days_data$Date, two_days_data$Time, sep = " "), format = "%Y-%m-%d %H:%M:%S"))
+
+# plot the graph
+png(file = "plot4.png", width = 480, height = 480, units = "px")
+par(mfcol = c(2,2), mar = c(4,4,2,1))
+
+#1st quadrant
+with(two_days_data,plot(posix, Global_active_power, type="l", xlab="", ylab="Global Active Power"))
+
+# 2nd quadrant
+with(two_days_data,plot(posix, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
+with(two_days_data,points(posix, Sub_metering_2, type="l", xlab="", col="red"))
+with(two_days_data,points(posix, Sub_metering_3, type="l", xlab="", col="blue"))
+legend("topright", col=c("black", "red","blue"), legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), lty=1)
+
+# 3rd quadrant
+with(two_days_data,plot(posix, Voltage, type="l", xlab="datetime", ylab="Voltage"))
+
+# 4th quadrant
+with(two_days_data,plot(posix, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power"))
+
+# close the png device
+dev.off()
